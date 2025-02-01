3a91a137931c4e890b91912526f76f2de2f919e9
David78-beto <davidhumberto031@gmail.com>
2025-02-01 17:38:59 -0500

    Clase 1
    Esta clase aprendimos a manejar y diferenciar interprete y un compilador, tambien que son las variables

---
+++ Primera Clase/Aprendizaje Python.py
@@ -0,0 +1,44 @@
+#Actividad 1
+"""Diferencia entre interprete y compilador
+----------------------------------------------------------------
+Interpretador Python:
+Se ejecuta directamente sobre el código fuente.
+Se puede corregir y ejecutar línea por línea.
+Puede ser usado para prototipado y desarrollo rápido.
+
+ejemplo:
+print ("Hola vida")#
+Hola vida
+----------------------------------------------------------------
+Compilador Python:
+Se compila el código fuente a bytecode.
+Este bytecode se ejecuta en un intérprete de Python.
+No se puede corregir y no se puede ejecutar línea por línea.""" 
+
+print ("Hola vida")
+print ("Estoy aprendiendo python")
+
+#----------------------------------------------------------------
+#Actvidad 2
+"""Que es una variable 
+Una variable es un espacio en la memoria de la 
+computadora donde se almacena un valor 
+almacenan texto y numeros
+el almacen de texto entre comillas
+diferencia de letras en mayusculas."""
+
+print("Esto es una suma")
+numero_uno = 2
+numero_dos = 5
+resultado = numero_uno + numero_dos
+print ("Su resultado es:",resultado)
+
+#----------------------------------------------------------------
+#Ejemplo para diferenciar mayusculas
+
+print ("Esto es una resta")
+Numero_uno = 9 
+numero_Uno = 8
+nUmero_uno ="hola vida"
+resultado = Numero_uno - numero_Uno 
+print ("el texto es:",nUmero_uno, "Su resultado es :", resultado )
---
+++ clases.md
@@ -0,0 +1,14 @@
+Actividad 1
+
+En python no se utiliza el punto y coma 
+#-----------------------------------------------------------
+Un compilador es un programa que traduce código fuente de un lenguaje de programación a código máquina para que la computadora lo ejecute.
+
+Un intérprete es un programa que traduce y ejecuta el código fuente línea por línea, sin generar un archivo de código máquina.
+#--------------------------------------------------------------
+Actividad 2
+que es una Variables
+es un epcacio de la memoria donde se guardan y se recuperan datos que utliza un programa
+Cada una debe tener un nombre en el cual pueda identificarse y referirse a ella.
+El nombre de una variable no puede coincidir con los nombres de los comandos asignados, este no debera contener espaciosen blanco, es decir que no se podria llamar print o nom bre 
+Los mas comunes son los que almacenan nombre texto (strings(str)) este siempre se encuentra entre comillas  o numeros (int o float)
